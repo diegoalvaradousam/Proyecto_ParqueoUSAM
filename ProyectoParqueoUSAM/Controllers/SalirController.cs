e6429622b2b116e479667fd2480a47abfb7a9e41
diegoalvaradousam <rhamphorychus.sthich@gmail.com>
2021-06-12 22:31:24 -0600

    enviando ultimos datos

---
+++ ProyectoParqueoUSAM/Controllers/SalirController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoParqueoUSAM.Controllers
+{
+    public class SalirController : Controller
+    {
+        // GET: Salir
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CerrarSesion()
+        {
+            Session["Usuario"] = null;
+            //return RedirectToAction("Ingresar","Accesos");
+            return RedirectToAction("Ingresar", "Accesos");
+        }
+
+    }
+}
